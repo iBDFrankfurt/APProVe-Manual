ff649f8271892fb42c0d22966f273396dcc59892
Timo Schneider <timo.schneider@kgu.de>
2022-07-21 09:05:29 +0200

    ### Added - vite parameters

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 import { searchPlugin } from "@vuepress/plugin-search";
-
+import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   base: "/manual/",
   port: 8586,
@@ -17,8 +17,19 @@ export default defineUserConfig({
     //   description: "Manual for APProVe - the tool for application, project and process management of iBDF",
     // },
   },
-
   theme,
+  bundler: viteBundler({
+    viteOptions: {
+      server: {
+        host: true,
+        port: 8586,
+        hmr:{
+          clientPort: 8586,
+        },
+      }
+    },
+    vuePluginOptions: {},
+  }),
   plugins: [
     searchPlugin({
       locales: {
