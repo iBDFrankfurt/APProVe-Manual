efe1e69295edfbe8423f07eb8bc0191812109e72
Timo Schneider <timo.schneider@kgu.de>
2022-07-22 10:46:58 +0200

    set dev server conf

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 import { searchPlugin } from "@vuepress/plugin-search";
 import { webpackBundler } from '@vuepress/bundler-webpack'
+import {createDevServerConfig} from "@vuepress/bundler-webpack/lib/dev/createDevServerConfig";
 export default defineUserConfig({
   base: "/",
   host: "0.0.0.0",
@@ -19,6 +20,12 @@ export default defineUserConfig({
   },
   theme,
   bundler: webpackBundler({
+    configureWebpack: (config, isServer) => {
+      config.devServer = {
+        host: "0.0.0.0",
+        hot: false
+      };
+    },
     postcss: {},
     vue: {
       isServerBuild: true,
