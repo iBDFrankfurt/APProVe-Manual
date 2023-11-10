79353e2f5223ad4ba21a14f635ec11e1bc29cb43
jaqueline.patzek@kgu.de <jaqueline.patzek@kgu.de>
2023-11-10 17:10:28 +0100

    moved navbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,18 +29,6 @@ export default defineUserConfig({
       "/updates/": "structure",
 
     },
-    navbar: [ "/README.md",
-              "/install/README.md",
-              "/user/README.md",
-              "/developing/README.md",
-              {
-                text: "Changelogs",
-                icon: "note",
-                prefix: "/updates/",
-                link: "/updates",
-                children: ["3-4-0", "3-3-0", "3-2-0", ""],
-              },
-            ],
   }),
   bundler: webpackBundler({
     configureWebpack: (config, isServer) => {
